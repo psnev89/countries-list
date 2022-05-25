ec8851969d46e2037a2ef54fb02e4e2845efbe96
Pedro Santana Neves <pneves@bladeinsight.com>
2022-05-25 23:50:30 +0100

    feat: add country list filters component

---
+++ src/modules/countries/components/CountryListFilters.vue
@@ -0,0 +1,53 @@
+<template>
+  <AppRow>
+    <AppColumn class="is-half">
+      <AppInputField
+        v-model="searchInput"
+        :disabled="disabled"
+        :loading="loading"
+        label="Search"
+        placeholder="Search by name or CCA3 code"
+      ></AppInputField>
+    </AppColumn>
+    <AppColumn>
+      <AppSelectField
+        :disabled="disabled"
+        :loading="loading"
+        label="Continent"
+      ></AppSelectField>
+    </AppColumn>
+    <AppColumn>
+      <AppSelectField
+        :disabled="disabled"
+        :loading="loading"
+        label="Sort By"
+      ></AppSelectField>
+    </AppColumn>
+  </AppRow>
+</template>
+
+<script>
+import AppRow from "@/components/app/grid/AppRow";
+import AppColumn from "@/components/app/grid/AppColumn";
+import AppInputField from "@/components/app/form/AppInputField";
+import AppSelectField from "@/components/app/form/AppSelectField";
+export default {
+  name: "CountryListFilters",
+  components: { AppSelectField, AppInputField, AppColumn, AppRow },
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      searchInput: "",
+    };
+  },
+};
+</script>
