00fa444707271587bcafb96bf08d16dc9bd66958
Pedro Santana Neves <pneves@bladeinsight.com>
2022-05-25 21:57:00 +0100

    feat: add app grid components

---
+++ src/components/app/grid/AppColumn.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="['column', colsClass]"><slot></slot></div>
+</template>
+
+<script>
+const minCols = 1;
+const maxCols = 12;
+const colsValidator = (value) => value >= minCols && value <= maxCols;
+export default {
+  name: "AppColumn",
+  props: {
+    cols: {
+      type: Number,
+      validator: colsValidator,
+    },
+  },
+  computed: {
+    colsClass() {
+      return colsValidator ? `is-${this.cols}` : "";
+    },
+  },
+};
+</script>
---
+++ src/components/app/grid/AppRow.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="['columns', { 'is-multiline': multiline }]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppRow",
+  props: {
+    multiline: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
