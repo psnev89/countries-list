8bd8e14ea042f606333ecded597d44561a724c3f
Pedro Santana Neves <pneves@bladeinsight.com>
2022-05-25 21:56:47 +0100

    feat: add app icon and notification component

---
+++ src/components/app/AppNotification.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :class="`notification ${typeClass}`">
+    <button v-if="hasDelete" class="delete"></button>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+const allowedTypes = [
+  "primary",
+  "link",
+  "info",
+  "success",
+  "warning",
+  "danger",
+];
+export default {
+  name: "AppNotification",
+  props: {
+    hasDelete: {
+      type: Boolean,
+      default: false,
+    },
+    text: {
+      type: String,
+    },
+    type: {
+      type: String,
+      validator: (value) => allowedTypes.includes(value),
+      default: "primary",
+    },
+  },
+  computed: {
+    typeClass() {
+      return allowedTypes.includes(this.type)
+        ? `is-${this.type}`
+        : "is-primary";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/app/icon/AppIcon.vue
@@ -0,0 +1,46 @@
+<template>
+  <span :class="`mdi mdi-${icon} --is-${sizeModifier}`"></span>
+</template>
+
+<script>
+const sizeMapper = {
+  16: "sm",
+  24: "md",
+  32: "lg",
+  40: "xl",
+};
+const allowedSizes = Object.keys(sizeMapper);
+const sizeValidator = (size) => allowedSizes.includes(`${+size}`);
+export default {
+  name: "AppIcon",
+  props: {
+    icon: { type: String, default: "help" },
+    size: {
+      type: [Number, String],
+      default: "24",
+      validator: sizeValidator,
+    },
+  },
+  computed: {
+    sizeModifier() {
+      const _size = sizeValidator(this.size) ? this.size : "24";
+      return sizeMapper[_size] || sizeMapper[24];
+    },
+  },
+};
+</script>
+
+<style scoped>
+.--is-md {
+  font-size: 24px;
+}
+.--is-sm {
+  font-size: 16px;
+}
+.--is-lg {
+  font-size: 32px;
+}
+.--is-xl {
+  font-size: 40px;
+}
+</style>
