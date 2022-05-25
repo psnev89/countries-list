b37fa4488da58bf8524e2d0c3b1046f24ce11184
Pedro Santana Neves <pneves@bladeinsight.com>
2022-05-25 23:49:18 +0100

    feat: add app form input and select components

---
+++ src/components/app/form/AppInputField.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+defineProps({
+  label: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: "text",
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  modelValue: {
+    type: String,
+    default: "",
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const update = (event) => {
+  emit("update:modelValue", event.target.value);
+};
+</script>
+
+<template>
+  <div class="field">
+    <label v-if="label" class="label">{{ label }}</label>
+    <div :class="['control', { 'is-loading': loading }]">
+      <input
+        class="input"
+        :type="type"
+        :placeholder="placeholder"
+        :disabled="disabled"
+        :value="modelValue"
+        @input="update"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+label {
+  font-size: 14px;
+}
+</style>
---
+++ src/components/app/form/AppSelectField.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="field">
+    <label v-if="label" class="label">{{ label }}</label>
+    <div class="control">
+      <div :class="['select', { 'is-loading': loading }]">
+        <select :disabled="disabled">
+          <template v-if="hasOptions">
+            <option>teste</option>
+          </template>
+          <option v-else disabled>No options available</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppSelectField",
+  props: {
+    options: {
+      type: Array,
+      default: () => [],
+    },
+    label: {
+      type: String,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    hasOptions() {
+      return this.options.length > 0;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+label {
+  font-size: 14px;
+}
+
+select,
+.select {
+  width: 100%;
+}
+</style>
